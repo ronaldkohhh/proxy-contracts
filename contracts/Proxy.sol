2193c490d0e075c0e8c4cc0053e18a682be897ab
ronaldkohhh <ronald9872@gmail.com>
2023-03-27 00:58:39 +0800

    first commit

---
+++ contracts/Proxy.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// import "hardhat/console.sol"
+
+// EOA -> Proxy -> Logic1
+//              -> Logic2
+
+import "./StorageSlot.sol";
+
+contract Proxy {
+    function changeImplementation(address _implementation) external {
+        StorageSlot.getAddressSlot(keccak256("impl")).value = _implementation;
+    }
+
+    fallback() external {
+        (bool success, ) =  StorageSlot.getAddressSlot(keccak256("impl")).value.delegatecall(msg.data);
+        require(success);
+    }
+}
+
+contract Logic1 {
+    uint x = 0;
+
+    function changeX(uint _x) external {
+        x = _x;
+    }
+}
+
+contract Logic2 {
+    uint x = 0;
+
+    function changeX(uint _x) external {
+        x = _x;
+    }
+
+    function tripleX() external {
+        x *= 3;
+    }
+}
